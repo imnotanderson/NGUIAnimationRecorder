1f6e3099c1c9a351eb7306c0be7deb9b4316a855
jian.z <36326068@qq.com>
2016-01-20 14:55:41 +0800

    up

---
+++ Editor/UIWidgetRecordInspector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+
+[CustomEditor(typeof(UIWidgetRecord))]
+public class UIWidgetRecordInspector : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var wr = (UIWidgetRecord)this.target;
+        if (wr.mWidth < 2)
+        {
+            wr.mWidth = 2;
+        }
+        if (wr.mHeight < 2)
+        {
+            wr.mHeight = 2;
+        }
+        GUILayout.BeginHorizontal();
+        float width = EditorGUILayout.IntField("Size",(int) wr.mWidth, GUILayout.MinWidth(150f));
+        EditorGUIUtility.labelWidth = 12f;
+        float height = EditorGUILayout.FloatField("x", (int)wr.mHeight, GUILayout.MinWidth(30f));
+        UnityEditor.Undo.RecordObject(wr, "wr");
+        wr.mWidth = (width);
+        wr.mHeight = (height);
+        wr.OnRenderObject();
+        if (GUILayout.Button("Snap", GUILayout.Width(60f)))
+        {
+            wr.MakePixelPerfect();
+        }
+        GUILayout.EndHorizontal();
+    }
+}
---
+++ UIWidgetRecord.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[ExecuteInEditMode]
+public class UIWidgetRecord : MonoBehaviour
+{
+    [HideInInspector]
+    public float mWidth, mHeight;
+    float tmWidth, tmHeight;
+
+    UIWidget mWidget = null;
+
+    UIWidget widget
+    {
+        get
+        {
+            if (mWidget == null)
+            {
+                mWidget = GetComponent<UIWidget>();
+                if (mWidget != null)
+                {
+                    mWidth = mWidget.width;
+                    mHeight = mWidget.height;
+                }
+            }
+            return mWidget;
+        }
+    }
+
+    void UpdateWidth()
+    {
+        if (tmWidth == mWidth)
+        {
+            return;
+        }
+        if (widget == null)
+        {
+            return;
+        }
+        if (widget.width != (int)mWidth)
+        {
+            widget.width = (int)(mWidth);
+            tmWidth = mWidth;
+        }
+    }
+
+    void UpdateHeight()
+    {
+        if(tmHeight == mHeight)
+        {
+            return;
+        }
+        if (widget == null) return;
+        if (widget.height != (int)mHeight)
+        {
+            widget.height = (int)(mHeight);
+            tmHeight = mHeight;
+        }
+    }
+
+
+    public void MakePixelPerfect()
+    {
+        if (widget == null) return;
+        if (widget is UISprite)
+        {
+            UISprite sprite = widget as UISprite;
+            UISpriteData sp = sprite.GetAtlasSprite();
+            if (sp == null) return;
+
+            Texture tex = sprite.mainTexture;
+            if (tex == null) return;
+
+            if (sprite.type == UISprite.Type.Simple || sprite.type == UISprite.Type.Filled || !sp.hasBorder)
+            {
+                if (tex != null)
+                {
+                    int x = Mathf.RoundToInt(sprite.pixelSize * (sp.width + sp.paddingLeft + sp.paddingRight));
+                    int y = Mathf.RoundToInt(sprite.pixelSize * (sp.height + sp.paddingTop + sp.paddingBottom));
+
+                    if ((x & 1) == 1) ++x;
+                    if ((y & 1) == 1) ++y;
+
+                    mWidth = x;
+                    mHeight = y;
+                }
+            }
+        }
+    }
+
+    void UpdateSize()
+    {
+        UpdateWidth();
+        UpdateHeight();
+    }
+
+    public void OnRenderObject()
+    {
+        UpdateSize();
+    }
+
+
+}
